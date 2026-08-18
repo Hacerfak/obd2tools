d96645016a28dba4a4d6f1877d48773a67209c30
Eder Gross Cichelero <hacerfak@outlook.com>
2026-08-17 21:54:11 -0300

    V1.0.0

--- lib/widgets/admob_banner.dart
+++ lib/widgets/admob_banner.dart
@@ -18,8 +18,8 @@ class _AdMobBannerState extends State<AdMobBanner> {
   Orientation? _currentOrientation;
 
   final String _adUnitId = Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/6300978111'
-      : 'ca-app-pub-3940256099942544/2934735716';
+      ? 'ca-app-pub-4241608895500197/7309130446'
+      : 'ca-app-pub-4241608895500197/7562768546';
 
   @override
   void didChangeDependencies() {
